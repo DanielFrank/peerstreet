82d4c5e8a0147d339fbc124d66f28fd1bc72aa4a
Daniel Frank <daniel.frank@lin-digital.com>
2018-04-20 19:04:23 -0700

    Add error checking and inputting zips to client

--- src/client.rb
+++ src/client.rb
@@ -1,8 +1,31 @@
 require 'net/http'
 require 'json'
-url = 'http://127.0.0.1:5000/zipToDMA/90266'
-uri = URI(url)
-response = Net::HTTP.get(uri)
-d=JSON.parse(response)
-result_string = "Zip: %s\nCBSA: %s\nMSA: %s\nPop2014: %d\nPop2015: %d"
-puts result_string % [d["Zip"], d["CBSA"], d["MSA"], d["Pop2014"], d["Pop2015"]]
+
+def check_exit(info)
+    return info == 'X' || info =='x'
+end
+
+def call_api(zip)
+    url = 'http://127.0.0.1:5000/zipToDMA/' + zip
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    d=JSON.parse(response)
+    if d.key?("error")
+        puts d["error"]
+        return
+    end
+    result_string = "Zip: %s\nCBSA: %s\nMSA: %s\nPop2014: %d\nPop2015: %d"
+    puts result_string % [d["Zip"], d["CBSA"], d["MSA"], d["Pop2014"], d["Pop2015"]]
+end
+
+zip = ''
+while (true)
+    puts "Enter a zip: (X to exit)"
+    zip = gets.chomp
+    break if check_exit(zip)
+    call_api(zip)
+end
+
+
+
+
