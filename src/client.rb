63329207f0eea2b143d5f899f5e9d800b4082d4c
Daniel Frank <daniel.frank@lin-digital.com>
2018-04-22 19:43:25 -0700

    Modify client to call public API and fix bug if pop is N/A

--- src/client.rb
+++ src/client.rb
@@ -5,8 +5,15 @@ def check_exit(info)
     return info == 'X' || info =='x'
 end
 
+def check_pop(pop)
+    if pop.is_a? Integer
+        return "%d"
+    end
+    return "%s"
+end
+
 def call_api(zip)
-    url = 'http://127.0.0.1:5000/zipToDMA/' + zip
+    url = 'http://ec2-52-35-55-238.us-west-2.compute.amazonaws.com/zipToDMA/' + zip
     uri = URI(url)
     response = Net::HTTP.get(uri)
     d=JSON.parse(response)
@@ -14,7 +21,9 @@ def call_api(zip)
         puts d["error"]
         return
     end
-    result_string = "Zip: %s\nCBSA: %s\nMSA: %s\nPop2014: %d\nPop2015: %d"
+    result_string = "Zip: %s\nCBSA: %s\nMSA: %s\nPop2014: " +
+        check_pop(d["Pop2014"]) + "\nPop2015: " +
+        check_pop(d["Pop2015"]) 
     puts result_string % [d["Zip"], d["CBSA"], d["MSA"], d["Pop2014"], d["Pop2015"]]
 end
 
